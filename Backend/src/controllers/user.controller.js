0cde487dfbc99d0ffd11ac8b55f31c82f4ee6575
sandeepverma <sv21022008@gmail.com>
2026-06-21 21:11:08 +0530

    fix: updated cookie options for production cross-domain

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -119,7 +119,8 @@ const loginUser = asyncHandler(async (req,res)=>{
 
    const options= {
      httpOnly :true,
-     secure:true     
+     secure:true ,
+     sameSite: "None",    
    }
 
    return res
