c649f3f2a5f81ed4ffaf710440fa2fe88a585419
sandeepverma <sv21022008@gmail.com>
2026-06-21 20:43:34 +0530

    fix: auto create public temp folder for multer

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,15 +1,22 @@
 import multer from "multer";
+import fs from "fs"; //  Ye import karna zaroori hai files handle karne ke liye
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, "./public/temp")
+        const dir = "./public/temp";
+        
+        //  Agar Render server par ye folder nahi milega, toh ye line khud naya folder bana degi
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        
+        cb(null, dir);
     },
     filename: function (req, file, cb) {
-      
-      cb(null, file.originalname)
+        cb(null, file.originalname);
     }
-  })
+});
   
 export const upload = multer({ 
     storage, 
-})
+});
